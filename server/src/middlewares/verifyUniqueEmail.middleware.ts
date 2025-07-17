6d5e22626bd1753edfe93f5b942dc5181eb36551
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-17 15:09:17 +0300

    implement the verifyUniqueEmail functionality

---
+++ server/src/middlewares/verifyUniqueEmail.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import client from "../utils/prismaClient.ts";
+import handleErrors from "../utils/handleErrors.ts";
+
+export default async function verifyUniqueEmail(req: Request, res: Response, next: NextFunction){
+    const {email} = req.body;
+    const {id} = res.locals.validUserData;
+
+    try{
+        const invalidEmail = await client.users.findFirst({
+            where: {
+                emailAddress: email,
+                NOT:{ id}
+            }
+        })
+        if(invalidEmail) return res.status(400).json({message: "Email already exists: Choose a unique and valid email"})
+        else {
+            res.locals.uniqueEmail = email;
+            next();
+        }
+    }catch(err){
+        handleErrors(err, "Update email later", req, res);
+    }
+}
--- server/src/middlewares/verifyUniqueUsername.middleware.ts
+++ server/src/middlewares/verifyUniqueUsername.middleware.ts
@@ -2,7 +2,7 @@ import { Response, Request, NextFunction } from "express";
 import client from "../utils/prismaClient.ts";  
 import handleErrors from "../utils/handleErrors.ts";
 
-export default async function verifyUniqueEmail(req: Request, res: Response, next: NextFunction){
+export default async function verifyUniqueUsername(req: Request, res: Response, next: NextFunction){
     const {userName} = req.body;
     const {id} = res.locals.validUserData;
     
