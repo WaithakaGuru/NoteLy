828fb24305672fe0450ea0799d8153db206d6bcf
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-18 11:20:12 +0300

    Add the note Router and its routes

--- server/src/routes/noteRouter.ts
+++ server/src/routes/noteRouter.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import {verifyJWebToken} from "../middlewares/exports.middleware.ts";
+import {deleteNote, getSpecificNote, restoreDeletedNote, updateNote} from "../controllers/exports.controller.ts";
 
 const noteRouter = Router();
-
-
+noteRouter.get("/:id", verifyJWebToken, getSpecificNote);
+noteRouter.patch("/:id", verifyJWebToken, updateNote);
+noteRouter.patch("/restore/:id", verifyJWebToken, restoreDeletedNote);
+noteRouter.delete("/:id", verifyJWebToken, deleteNote);
 export default noteRouter;
