b06b639803efe4625fb8ba4adc532142178fca4c
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-23 16:11:44 +0300

    add the NoNotesAddedYet default placeholder component

---
+++ client/src/components/NoNote.tsx
@@ -0,0 +1,26 @@
+import { Stack, Button, Typography } from "@mui/material"
+import { NoteAdd } from "@mui/icons-material"
+
+function NoNote() {
+  return (
+    <Stack
+        className="min-w-96  p-4 items-center gap-4 shadow-xl h-54 border-gray-300 border
+        rounded-xl justify-center bg-white"
+    >
+        <NoteAdd className="text-purple-900 text-3xl" />
+        <Typography variant="h5" gutterBottom className="text-gray-700">
+            No notes added yet
+        </Typography>
+        <Button
+            variant="contained"
+            color="secondary"
+            title="Add a new title"
+            href="/dashboard/create"
+        >
+            Create a new note
+        </Button>
+    </Stack>
+  )
+}
+
+export default NoNote
