1cca7ebb97a1597f9046e4b5c7dca0c19113c3de
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-25 15:59:41 +0300

    Add the delete client-side request services

---
+++ client/src/services/deleteRequests.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+import useNote from "../store/notelyStore"
+import ax from "../utils/axInstance";
+
+const useDeleteNote = (id: string)=> {
+    const {token} = useNote();
+    return useMutation({
+        mutationKey: ["DeleteNote", id],
+        mutationFn: async (data) => {
+            const deletedNote = ax.delete("/note/:id", {
+                headers: {Authorization: `Author ${token}`}
+            })
+            return deletedNote
+        }
+    })
+}
+export default useDeleteNote;
