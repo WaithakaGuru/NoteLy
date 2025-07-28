ba58d211c0eac2f234c97c1c5dd71ab2299e4fe3
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-28 12:56:55 +0300

    update scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   CORS({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://note-ly-xi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
