1b7245d22fc10b3016c143c8dc7b329d8de868a2
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-28 13:25:53 +0300

    Update axiosInstance base URL

--- client/src/utils/axInstance.ts
+++ client/src/utils/axInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const URL = "https://notely-server-r48z.onrender.com/api";
 const ax = axios.create({
-  baseURL: "http://localhost:5567/api",
+  baseURL: URL, 
   withCredentials: true,
 });
 
